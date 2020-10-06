bf050f912b07359e30b3491e90601bac521ca6b5
cristianekoshino <cristianekoshino@gmail.com>
2020-10-06 16:51:38 +0100

    trabalho-pratico-2 created

---
+++ modulo2/trabalho-pratico/galeria-fotos/src/app/galeria-component/galeria-component.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria-component',
+  templateUrl: './galeria-component.component.html',
+  styleUrls: ['./galeria-component.component.css']
+})
+export class GaleriaComponentComponent {
+  @Input() title: string = '';
+  @Input() pictures?: string[] = [];
+  counter: number = 0;
+  show: boolean = true;
+
+  changePicture(value:number) {
+    console.log(this.counter + 1, this.pictures.length)
+    this.counter = this.counter + value;
+
+    if(this.counter + 1 === this.pictures.length) {
+      console.log('igual', this.counter + 1, this.pictures.length)
+      this.counter = this.counter;
+    }
+  }
+
+  firstPicture() {
+    this.counter = 0;
+  }
+  
+  lastPicture() {
+    this.counter = this.pictures.length - 1;
+  }
+
+  
+}
